114278c1c98e2643ea00b4280e0fb1536f6f921d
Antony Videtta <antonyvidetta@catalyst.net.nz>
2021-08-06 11:07:04 +1200

    Fix issue with unit test failing

--- classes/grades/grader/gradingpanel/external/store.php
+++ classes/grades/grader/gradingpanel/external/store.php
@@ -96,17 +96,16 @@ class store extends external_api {
      * @param int $contextid
      * @param string $itemname
      * @param int $gradeduserid
-     * @param string $formdata
      * @param bool $notifyuser
+     * @param string $formdata
      * @return array
      * @throws coding_exception
      * @throws moodle_exception
      * @since Moodle 3.8
      */
-    public static function execute(string $component, int $contextid, string $itemname, int $gradeduserid, string $formdata,
-        bool $notifyuser): array {
+    public static function execute(string $component, int $contextid, string $itemname, int $gradeduserid, bool $notifyuser,
+    string $formdata): array {
         global $USER;
-
         [
             'component' => $component,
             'contextid' => $contextid,
